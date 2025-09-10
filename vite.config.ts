6f42f5993d095efc053f596d24023044ee7ff619
も。 <ddbw4720@yahoo.co.jp>
2025-09-10 19:45:50 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react'; // ★ Reactプラグインをインポートします
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // ↓↓↓ ここから2行追加します ↓↓↓
+      plugins: [react()],
+      base: "/Moneylabel/", 
+      // ↑↑↑ ここまで追加 ↑↑↑
+      
+      // 以下は元の設定です
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
