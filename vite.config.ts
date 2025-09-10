08a1913395f7fd2b26d6bc2f4b7a6f41043c6167
TOMIMOE1226 <tomimoe@outlook.jp>
2025-09-10 22:11:15 +0900

    fix: baseパスを修正し、ビルド時の正しいルートを設定しました

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,11 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'; // ★ Reactプラグインをインポートします
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+export default defineConfig(({ command }) => {
+    const env = loadEnv(command, '.', '');
     return {
-      // ↓↓↓ ここから2行追加します ↓↓↓
       plugins: [react()],
-      base: "./", 
-      // ↑↑↑ ここまで追加 ↑↑↑
+      base: command === 'build' ? "/Moneylabel/" : "/",
       
       // 以下は元の設定です
       define: {
